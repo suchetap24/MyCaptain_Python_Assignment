3f278fc77399e3b4f2d879fea4c54a186735e742
suchetap24 <118294291+suchetap24@users.noreply.github.com>
2023-02-11 09:08:10 +0530

    Add files via upload

---
+++ pos_num.py
@@ -0,0 +1,16 @@
+a=[12, -7, 5, 64, -14]
+b=[]
+for i in a:
+    if i>0:
+        b.append(i)
+print(b)   
+c=[12, 14, -95, 3] 
+d=[]
+for i in c:
+    if i>0:
+        d.append(i)
+print(d)        
+        
+        
+        
+
